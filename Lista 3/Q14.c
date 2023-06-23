4c854b8c86d4de5a5c64d1e4677cedb739abd85a
Pedro <107764107+pedrocsf@users.noreply.github.com>
2023-06-22 21:36:01 -0300

    Update and rename Q14.c to Lista 3/Q14.c

--- Lista 3/Q14.c
+++ Lista 3/Q14.c
@@ -20,31 +20,31 @@ int i, IDADE1, IDADE2, IDADE3, PESO1, PESO2, PESO3, IDADE_MEDIA=0, PESO_MEDIO=0,
 		scanf("%d", &PESO3);
 		
 		PESO_MEDIO = (PESO1+PESO2+PESO3)/3;
-		printf("Peso médio dos competidores da cidade %d: %d\n", i, PESO_MEDIO);
+		printf("Peso mÃ©dio dos competidores da cidade %d: %d\n", i, PESO_MEDIO);
 		
 		IDADE_MEDIA= (IDADE1+IDADE2+IDADE3)/3;
-		printf("Idade média dos competidores da cidade %d: %d\n", i, IDADE_MEDIA);
+		printf("Idade mÃ©dia dos competidores da cidade %d: %d\n", i, IDADE_MEDIA);
 		
 		if (PESO1 >= PESO2 && PESO1 >= PESO3) {
-        printf("O competidor 1 é o mais pesado\n");
+        printf("O competidor 1 Ã© o mais pesado\n");
     	} else if (PESO2 >= PESO1 && PESO2 >= PESO3) {
-        printf("O competidor 2 é o mais pesado\n");
+        printf("O competidor 2 Ã© o mais pesado\n");
     	} else {
-        printf("O competidor 3 é o mais pesado\n");
+        printf("O competidor 3 Ã© o mais pesado\n");
     	}
     	
     	if (IDADE1 <= IDADE2 && IDADE1 <= IDADE3) {
-        printf("O competidor 1 é o mais jovem\n");
+        printf("O competidor 1 Ã© o mais jovem\n");
     	} else if (IDADE2 <= IDADE3 && IDADE2 <= IDADE3) {
-        printf("O competidor 2 é o mais jovem\n");
+        printf("O competidor 2 Ã© o mais jovem\n");
     	} else {
-        printf("O competidor 3 é o mais jovem\n");
+        printf("O competidor 3 Ã© o mais jovem\n");
     	}
     	PESO_TOTAL=PESO_TOTAL+PESO1+PESO2+PESO3;
     	IDADE_TOTAL=IDADE1+IDADE2+IDADE3;
 	}
 	PESO_MEDIO = PESO_TOTAL/15;
 	IDADE_MEDIA = IDADE_TOTAL/15;
-	printf("Peso médio de todos os participantes: %d\nIdade média de todos os participantes: %d", PESO_MEDIO, IDADE_MEDIA);
+	printf("Peso mÃ©dio de todos os participantes: %d\nIdade mÃ©dia de todos os participantes: %d", PESO_MEDIO, IDADE_MEDIA);
 return 0;
 }
